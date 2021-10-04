2c8b33a9c6a9d1b34b65a5e930be671dca8a11e7
AnnaZaragoza <74377943+AnnaZaragoza@users.noreply.github.com>
2021-10-04 19:20:08 +0200

    Add files via upload

--- accountancies/script3(editTableAndSumValues).js
+++ accountancies/script3(editTableAndSumValues).js
@@ -1,6 +1,9 @@
 'use strict'
 
-function editTds (tableId){
+//var contentOfTd = tdEditable.getElementById("td-editable").textContent;
+//console.log(contentOfTd);
+
+function editTds (tableId, colNumber){
   var table = document.getElementById(tableId);
   var tableBody = table.getElementsByTagName('tbody').item(0);
   var tds = tableBody.querySelectorAll('tr td:nth-last-child(-n+3)'); // allow editing last three tds only
@@ -24,60 +27,26 @@ function editTds (tableId){
         e.preventDefault();
       }
     });
+
+    // sum content NOT WORKING!!
+    /*
+    tds.forEach (td => {
+      td.addEventListener('change', function(){
+        var sum = 0;
+        for(var i=0; i<tds.length; i++){
+          sum += parseFloat(tds[i].data);
+        }
+        table.getElementsByTagName("tfoot").item(0).rows[0].cells[colNumber].innerHTML = sum;
+        console.log(sum);
+      });
+    });
+    */
   }
 }
 
-editTds("twenty-one");
+editTds("twenty-one", 1);
+editTds("twenty-one", 2);
+editTds("twenty-one", 3);
 editTds("twenty");
 editTds("nineteen");
 editTds("eighteen");
-
-// Trying to integrate this function when the content of the tds are changing:
-function sumColumnAndShowTotal(tableId, colNumber){
-  var total = 0;
-
-   var table = document.getElementById(tableId);
-   var tableBody = table.getElementsByTagName("tbody").item(0);
-   var i;
-   var rows = tableBody.rows.length;
-
-   for (i=0; i<rows; i++){
-     var thisTrBody = tableBody.rows[i];
-     var thisTdBody = thisTrBody.cells[colNumber];
-     var thisTextNode = thisTdBody.childNodes.item(0);
-
-     var thisNumber = parseFloat(thisTextNode.data);
-     total += thisNumber;
-   }
-
-   table.getElementsByTagName("tfoot").item(0).rows[0].cells[colNumber].innerHTML = total;
-}
-
-
-// Trying to convert tds elements into inputs:
-/*
-var table = document.getElementById('twenty-one');
-var tableBody = table.getElementsByTagName('tbody').item(0);
-var tds = tableBody.querySelectorAll('tr td:nth-last-child(-n+3)'); //allow editing last three tds only
-
-for(var i=0; i<tds.length; i++) {
-  tds[i].addEventListener("click", ()=>{
-    console.log('you clicked me!');
-    // change <td> element per <input> element:
-    /*
-    var replaceElement = document.getElementById('td-editable-as-input');
-    var newElement = document.createElement('input');
-    newElement.innerHTML = '<input type="number">';
-    replaceElement.parentNode.replaceChild(newElement, replaceElement);
-    */
-    // change <td> elements per <input> elements: (NOT WORKING)
-    /*
-    var replaceElements = document.getElementsByTagName('td');
-    var newElements = document.createElement('input');
-    newElements.innerHTML = '<input>';
-    replaceElements.parentNode.replaceChild(newElements, replaceElements);
-    */
-    /*
-  });
-}
-*/
