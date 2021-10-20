7f9f52c9d0e0add036746a6b8c55db3f97dcca9f
AnnaZaragoza <74377943+AnnaZaragoza@users.noreply.github.com>
2021-10-20 08:28:35 +0200

    Update script3(editTableAndSumValues).js

--- accountancies/script3(editTableAndSumValues).js
+++ accountancies/script3(editTableAndSumValues).js
@@ -1,16 +1,16 @@
 'use strict'
 
 function editTableAndSumValues(tableId){
-  var table = document.getElementById(tableId);
+  const table = document.getElementById(tableId);
 
-  var inputsOne = table.querySelectorAll('.editable-field-one');
-  var inputsTwo = table.querySelectorAll('.editable-field-two');
-  var inputsThree = table.querySelectorAll('.editable-field-three');
+  const inputsOne = table.querySelectorAll('.editable-field-one');
+  const inputsTwo = table.querySelectorAll('.editable-field-two');
+  const inputsThree = table.querySelectorAll('.editable-field-three');
 
   inputsOne.forEach (input => {
     input.addEventListener('change', function(){
-      var sum = 0;
-      for(var i=0; i<inputsOne.length; i++){
+      let sum = 0;
+      for(let i=0; i<inputsOne.length; i++){
         sum += parseFloat(inputsOne[i].value);
       }
       table.getElementsByTagName("tfoot").item(0).rows[0].cells[1].innerHTML = sum;
@@ -34,8 +34,8 @@ function editTableAndSumValues(tableId){
 
   inputsTwo.forEach (input => {
     input.addEventListener('change', function(){
-      var sum = 0;
-      for(var i=0; i<inputsTwo.length; i++){
+      let sum = 0;
+      for(let i=0; i<inputsTwo.length; i++){
         sum += parseFloat(inputsTwo[i].value);
       }
       table.getElementsByTagName("tfoot").item(0).rows[0].cells[2].innerHTML = sum;
@@ -59,8 +59,8 @@ function editTableAndSumValues(tableId){
 
   inputsThree.forEach (input => {
     input.addEventListener('change', function(){
-      var sum = 0;
-      for(var i=0; i<inputsThree.length; i++){
+      let sum = 0;
+      for(let i=0; i<inputsThree.length; i++){
         sum += parseFloat(inputsThree[i].value);
       }
       table.getElementsByTagName("tfoot").item(0).rows[0].cells[3].innerHTML = sum;
@@ -90,24 +90,24 @@ editTableAndSumValues("eighteen");
 
 // Trying to convert tds elements into inputs:
 /*
-var table = document.getElementById('twenty-one');
-var tableBody = table.getElementsByTagName('tbody').item(0);
-var tds = tableBody.querySelectorAll('tr td:nth-last-child(-n+3)'); //allow editing last three tds only
+const table = document.getElementById('twenty-one');
+const tableBody = table.getElementsByTagName('tbody').item(0);
+const tds = tableBody.querySelectorAll('tr td:nth-last-child(-n+3)'); //allow editing last three tds only
 
-for(var i=0; i<tds.length; i++) {
+for(let i=0; i<tds.length; i++) {
   tds[i].addEventListener("click", ()=>{
     console.log('you clicked me!');
     // change <td> element per <input> element:
     /*
-    var replaceElement = document.getElementById('td-editable-as-input');
-    var newElement = document.createElement('input');
+    const replaceElement = document.getElementById('td-editable-as-input');
+    const newElement = document.createElement('input');
     newElement.innerHTML = '<input type="number">';
     replaceElement.parentNode.replaceChild(newElement, replaceElement);
     */
     // change <td> elements per <input> elements: (NOT WORKING)
     /*
-    var replaceElements = document.getElementsByTagName('td');
-    var newElements = document.createElement('input');
+    const replaceElements = document.getElementsByTagName('td');
+    const newElements = document.createElement('input');
     newElements.innerHTML = '<input>';
     replaceElements.parentNode.replaceChild(newElements, replaceElements);
     */
